407ee431c80e76885117434f1d484a7503e05383
Muhammad Alfiandi Rizki <alfian.jw@gmail.com>
2026-03-09 16:44:01 +0700

    fix : fix api to proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  preview: {
-    host: true,
-    allowedHosts: ['dev-dashboard.pakargaming.id','dashboard.pakargaming.id'],
-  },
-})
+    preview: {
+      host: true,
+      allowedHosts: [
+        "dev-dashboard.pakargaming.id",
+        "dashboard.pakargaming.id",
+      ],
+    },
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+          configure: (proxy) => {
+            proxy.on("proxyReq", (proxyReq) => {
+              proxyReq.removeHeader("X-Forwarded-For");
+            });
+          },
+        },
+      },
+    },
+  };
+});
