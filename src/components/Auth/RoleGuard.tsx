c6806a11b27cd17f88f0255e7e9cf958d1979d55
Muhammad Alfiandi Rizki <alfian.jw@gmail.com>
2026-03-16 15:03:32 +0700

    feat(guard) : add validation token

--- src/components/Auth/RoleGuard.tsx
+++ src/components/Auth/RoleGuard.tsx
@@ -9,6 +9,12 @@ interface RoleGuardProps {
 export const RoleGuard = ({ children, allowedRoles }: RoleGuardProps) => {
   const { role } = useAuthUser();
 
+  const token = localStorage.getItem("access_token");
+
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
+
   if (!role || !allowedRoles.includes(role)) {
     return <Navigate to="/unauthorized" replace />;
   }
