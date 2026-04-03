b7a39672ddf122267b924e2ce8562c7d9be964cc
Muhammad Alfiandi Rizki <alfian.jw@gmail.com>
2026-04-04 01:17:44 +0700

    feat : 2fa adjust ui/ux

--- src/pages/SetupAuth.tsx
+++ src/pages/SetupAuth.tsx
@@ -1,10 +1,12 @@
-import Setup2FA from "@/components/Auth/Setup2Fa";
-import { DashboardLayout } from "@/components/Layout/dashboard-layout";
+import Setup2FA from '@/components/Auth/Setup2Fa'
+import { DashboardLayout } from '@/components/Layout/dashboard-layout'
 
 export default function Setup2FAPage() {
   return (
     <DashboardLayout>
-      <Setup2FA />
+      <div className="mx-auto max-w-7xl space-y-6">
+        <Setup2FA />
+      </div>
     </DashboardLayout>
-  );
+  )
 }
